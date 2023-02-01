9ac35652b0b61cff254939b385f1e154c74a9341
Lauren <halpertln@gmail.com>
2023-02-01 13:51:31 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = array => [array[0], array[1]];
+//returnFirstTwoDrivers();
+
+const returnLastTwoDrivers = array => [array[array.length-2], array[array.length-1]];
+//returnLastTwoDrivers();
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (integer) {
+    return function (fare) {
+        return fare * integer;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (array, fn) {
+    return fn(array);
+}
